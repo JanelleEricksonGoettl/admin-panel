44c53eb68d0b7c6edb11bd2610f978a4355fd37e
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-10 15:11:42 -0500

    created students controller and added nav to application html template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  # get 'students/index'
+  # get 'students/new'
+  # get 'students/edit'
+  # get 'students/update'
+  # get 'students/create'
+  # get 'students/destroy'
+  # get 'students/show'
+
+  
+  get 'home/index'
   root to: "home#index"
 
   devise_for :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 
 end
